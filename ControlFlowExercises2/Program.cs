88fd02e3787f97a66e8314e88e20917e26780605
glopezfricker <glopezfricker@gmail.com>
2020-07-17 00:52:54 -0300

    Ejercicio 2 controlflow

---
+++ ControlFlowExercises2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ControlFlowExercises2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program which takes two numbers from the console and displays the maximum of the two.
+            string input1, input2;
+            int num1, num2;
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Ingrese el primer numero:");
+                    input1 = Console.ReadLine();
+                    num1 = Convert.ToInt32(input1);
+
+                    Console.Write("Ingrese el segundo numero:");
+                    input2 = Console.ReadLine();
+                    num2 = Convert.ToInt32(input2);
+                    break;
+                }
+                catch(SystemException e)
+                {
+                    Console.WriteLine("No ingreso un numero valido");
+                }
+
+            }
+            
+            if(num1 > num2)
+            {
+                Console.WriteLine(num1);
+            } else if(num2 > num1){
+                Console.WriteLine(num2);
+
+            } else
+            {
+                Console.WriteLine("Son iguales");
+            }
+        }
+    }
+}
