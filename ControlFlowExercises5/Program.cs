bfca9b6191f33cd4119800dc30cdaddf031acd83
glopezfricker <glopezfricker@gmail.com>
2020-07-17 02:59:25 -0300

    Ejercicio 5 controlflow

---
+++ ControlFlowExercises5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ControlFlowExercises5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int res = 0;
+            //Write a program to count how many numbers between 1 and 100 are divisible by 3 with no remainder.Display the count on the console.
+            for(int i = 1; i <= 100; i++)
+            {
+                if(i % 3 == 0)
+                {
+                    res++;
+                }
+            }
+            Console.WriteLine(res);
+        }
+    }
+}
