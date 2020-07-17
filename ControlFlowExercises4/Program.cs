520e9bf6b0485fbc245c0ae33b942e7258e138c2
glopezfricker <glopezfricker@gmail.com>
2020-07-17 01:21:58 -0300

    Ejercicio 3 controlflow

---
+++ ControlFlowExercises4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ControlFlowExercises4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program and ask the user to enter the width and height of an image.Then tell if the image is landscape or portrait.
+            int ancho, largo;
+            string input1, input2;
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Ingrese el largo de la imagen:");
+                    input1 = Console.ReadLine();
+                    largo = Convert.ToInt32(input1);
+
+                    Console.Write("Ingrese el ancho de la imagen:");
+                    input2 = Console.ReadLine();
+                    ancho = Convert.ToInt32(input2);
+                    break;
+                }
+                catch (SystemException e)
+                {
+                    Console.WriteLine("No ingreso un numero valido");
+                }
+
+            }
+
+            if (largo > ancho)
+            {
+                Console.WriteLine("Es portrait");
+            }
+            else if (ancho > largo)
+            {
+                Console.WriteLine("Es landscape");
+
+            }
+            else
+            {
+                Console.WriteLine("Son iguales");
+            }
+        }
+    }
+}
+
