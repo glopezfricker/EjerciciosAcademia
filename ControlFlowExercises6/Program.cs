d78aad38fc6e292ee8ca9e886924a6ed6e81758a
glopezfricker <glopezfricker@gmail.com>
2020-07-23 14:15:27 -0300

    Ejercicio 1 Working with text

---
+++ ControlFlowExercises6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ControlFlowExercises6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program and continuously ask the user to enter a number or "ok" to exit. 
+            //Calculate the sum of all the previously entered numbers and display it on the console.
+            Console.ReadLine("Ingrese un numero (ok para terminar):");
+            string input = Console.ReadLine();
+            while(input != "ok")
+            {
+
+            }
+        }
+    }
+}
---
+++ FilesExercises1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace FilesExercises1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1-Write a program that reads a text file and displays the number of words.
+            //Lo unico que no me gusta es que si hay una coma no cuenta como 2 palabras sino como 1 sola.
+            var currentDir = Directory.GetCurrentDirectory();
+            var textFile = currentDir + @"/texto.txt";
+            int Total = ContarPalabras(textFile);
+
+            if(Total < 0)
+            {
+                Console.WriteLine("El archivo no existe.");
+            } else
+            {
+                Console.WriteLine("El total es: {0}", Total);
+            }
+
+
+        }
+
+        public static int ContarPalabras(string FilePath = "") {
+            
+            if (!File.Exists(FilePath))
+            {
+                return -1;
+            }
+            else
+            {
+                var TextoCompleto = File.ReadAllText(FilePath);
+                var ArregloTexto = TextoCompleto.Split(" ");
+                return ArregloTexto.Length;
+            }
+
+        }
+    }
+}
